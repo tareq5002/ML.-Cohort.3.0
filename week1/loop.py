e46c8be3293a12afa65c6aebbe13c39c56a4ad9d
Hasibur Rahman <hasiburrahmantareq@gmail.com>
2025-06-17 00:44:01 +0600

    Add files via upload
    Code with a loop function ....

---
+++ week1/loop.py
@@ -0,0 +1,28 @@
+## simple for loop using
+
+fruits = ["mango", "banana", "lichi"]
+
+for x in fruits:          ## fruits index in the X variable
+    print(x)
+
+## range code
+
+for x in range (11):
+    print(x) 
+
+    ## nested for loop
+
+
+number = 1
+
+for i in range (3):
+
+        for j in range (3):
+            print(number, end = " ")
+
+            number += 1
+            print()
+
+
+## next task is submitted by While loop
+
